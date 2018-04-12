4c0396a058478f2b7ea773b3d3de780f222ef65b
赵可 <zhaokep@yonyou.conm>
2018-04-12 21:34:02 +0800

    初始化工程

---
+++ src/main/java/com/enterprise/PromServiceApplication.java
@@ -0,0 +1,12 @@
+package com.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromServiceApplication.class, args);
+	}
+}
